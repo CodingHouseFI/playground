d9a021dd190f061b9da6f21024bd05efaab3aba2
Collin Webb <collindwebb@gmail.com>
2015-06-10 11:56:46 -0700

    fixed cloning and DOM

--- karma/script.js
+++ karma/script.js
@@ -1,22 +1,25 @@
 var data = [
-  { name: 'Ryan', points: 14},
-  { name: 'Colin', points: 7},
-  { name: 'Tania', points: 9},
-  { name: 'Nick', points: 21}
+  { name: 'Ryan', points: 14 },
+  { name: 'Colin', points: 7 },
+  { name: 'Tania', points: 9 },
+  { name: 'Mike', points: 21 }
 ];
 
-var leaderboard2 = function(data) {
-  return data.sort(function(a,b){return b.points-a.points;})
-}
+var sortedData = data.sort(function(a, b){
+  return a.points < b.points;
+});
 
-function liCreate(name,points) {
-  var li = $('<li>'+name+'</li>');
-  li.append('<span>'+points+'</span>');
-}
 
-$(document).ready(function() {
-  var sorted = leaderboard2(data);
-  for (var i=0; i<sorted.length; i++) {
-    $('body').append('<div><p>'+sorted[i].name+' '+sorted[i].points+'</p></div>')
-  }
-})
+$(document).ready(function(){
+  var leaders = [];
+  var $cloneTemplate = $(".person:first").clone(true).show();
+
+  sortedData.forEach(function(data){
+    var $clone = $cloneTemplate.clone();
+    $clone.find(".name").text(data.name);
+    $clone.find(".points").text(data.points);
+    leaders.push($clone);
+  });
+
+  $("#leaderboard").append(leaders);
+});
