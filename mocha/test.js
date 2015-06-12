c782c330609af0ce2ff247177aeeb7873de2d53f
AgileLabs <samer@agilelabs.com>
2015-06-12 15:28:17 -0700

    TDD, take 4

--- mocha/test.js
+++ mocha/test.js
@@ -1,32 +1,16 @@
-var assert = require("assert")
-var CH = {};
-
-CH.sum = function(a, b) {
-  // assert.deepEqual([4, 8], CH.sum([2, 2], [3, 5]));
-  // a = [2,2] => 4 => sum(2,2) | sum.apply(null, [2,2])
-  // b = [3,5] => 8
-  // [4,8]
-
-  // CH.sum.apply(null, everyArrayArgument)
-  // CH.sum.apply(null, a)
-  // CH.sum.apply(null, b)
-  // [a, b] => [CH.sum.apply(null, a), CH.sum.apply(null, b)]
-
-  // return arguments.map(function(oneArg) {
-  //   return CH.sum.apply(null, oneArg);
-  // });
-  if (Array.isArray(arguments[0])) {
-    return Array.prototype.map.call(arguments, function(oneArg) {
-      return CH.sum.apply(null, oneArg);
-    });
-  }
-
-  return Array.prototype.reduce.call(arguments, function(total, e) {
-    return total + e || 0;
-  }, 0);
-}
+var assert = require("assert");
+var CH = require("./index.js");
 
 describe('CH', function() {
+  describe("Cade's funky operate", function() {
+    it("operates", function() {
+      assert.equal(4, CH.operate('+')(2,2));
+      assert.equal(1, CH.operate('-')(5,4));
+      assert.equal(12, CH.operate('*')(4,3));
+      assert.equal(2, CH.operate('/')(4,2));
+    });
+  });
+
   describe('the sum function', function() {
     it("returns the sum of its X arguments", function() {
       assert.equal(7, CH.sum(3, 4));
