2ed389ea275f8d17c229b991c41c4225042af0ce
AgileLabs <samer@agilelabs.com>
2015-06-13 13:15:34 -0700

    MVP, take 2

--- chat/js/main.js
+++ chat/js/main.js
@@ -1,17 +1,32 @@
-var ref = new Firebase("https://torrid-heat-146.firebaseio.com/son");
+var ref = new Firebase("https://torrid-heat-146.firebaseio.com/chat");
 
-ref.set({
-  author: "Samer",
-  message: "Hello World!",
+$("textarea").on("keyup", function(event) {
+  if (event.which === 13) {
+    sendMessage(this.value);
+    this.value = "";
+  }
 });
 
-ref.on("value", function(snapshot) {
-  console.log(snapshot.val());
+var sendMessage = function(textValue) {
+  if (textValue) {
+    ref.push({
+      author: "Samer",
+      message: textValue,
+      messageDate: Firebase.ServerValue.TIMESTAMP
+    });
+  }
+};
+
+ref.on("child_added", function(snapshot) {
   var newMessage = snapshot.val();
-  var messageDiv = $("<div>")
-                  .append($("<span>", { class: 'author', text: newMessage.author }))
-                  .append($("<span>", { text: newMessage.message }))
-  $("#chat").prepend(messageDiv);
+  if (newMessage.author && newMessage.message
+       && newMessage.message.trim().length > 0) {
+    var messageDiv = $("<div>")
+    .append($("<span>", { class: 'time', text: moment(newMessage.messageDate).format('lll') }))
+    .append($("<span>", { class: 'author', text: newMessage.author }))
+    .append($("<span>", { class: 'body', html: marked(newMessage.message) }))
+    $("#chat").prepend(messageDiv);
+  }
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
