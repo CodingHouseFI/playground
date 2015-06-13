d780f26c4c4043b5edb83720c2d10bba2f5a3834
AgileLabs <samer@agilelabs.com>
2015-06-13 15:51:28 -0700

    GH login, MVP

--- chat/js/main.js
+++ chat/js/main.js
@@ -1,33 +1,44 @@
 var ref = new Firebase("https://torrid-heat-146.firebaseio.com/chat");
 
-$("textarea").on("keyup", function(event) {
-  if (event.which === 13) {
-    sendMessage(this.value);
-    this.value = "";
+ref.authWithOAuthPopup("github", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+    initChat(authData.github);
   }
 });
 
-var sendMessage = function(textValue) {
-  if (textValue) {
-    ref.push({
-      author: "Samer",
-      message: textValue,
-      messageDate: Firebase.ServerValue.TIMESTAMP
-    });
-  }
-};
+var initChat = function(user) {
+  $("textarea").on("keyup", function(event) {
+    if (event.which === 13) {
+      sendMessage(this.value);
+      this.value = "";
+    }
+  });
 
-var $messageTemplate = $(".text:first");
+  var sendMessage = function(textValue) {
+    if (textValue) {
+      ref.push({
+        author: user.displayName,
+        message: textValue,
+        messageDate: Firebase.ServerValue.TIMESTAMP
+      });
+    }
+  };
 
-ref.on("child_added", function(snapshot){
-  var newMessage = snapshot.val();
-  if (newMessage.author && newMessage.message && newMessage.message.trim().length > 0){
-    var $messageDiv = $messageTemplate.clone().removeClass("hidden")
-    $messageDiv.find(".time").text(moment(newMessage.messageDate).format("lll"));
-    $messageDiv.find(".author").text(newMessage.author);
-    $messageDiv.find(".message").html(marked(newMessage.message));
-    $("#chat").prepend($messageDiv);
-  }
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
+  var $messageTemplate = $(".text:first");
+
+  ref.on("child_added", function(snapshot){
+    var newMessage = snapshot.val();
+    if (newMessage.author && newMessage.message && newMessage.message.trim().length > 0){
+      var $messageDiv = $messageTemplate.clone().removeClass("hidden")
+      $messageDiv.find(".time").text(moment(newMessage.messageDate).format("lll"));
+      $messageDiv.find(".author").text(newMessage.author);
+      $messageDiv.find(".message").html(marked(newMessage.message));
+      $("#chat").prepend($messageDiv);
+    }
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+}
