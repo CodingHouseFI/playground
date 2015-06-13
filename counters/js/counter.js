582deeb2f53d15c56a4c8e55638292979bd27ac0
AgileLabs <samer@agilelabs.com>
2015-06-13 10:17:35 -0700

    Paragraphs

--- counters/js/counter.js
+++ counters/js/counter.js
@@ -1,15 +1,20 @@
 var Counter = {}
 
+Counter.isWord = function(word) {
+  return word.length > 0;
+};
+
 Counter.count = function(text) {
+  var wordsMatch = text.match(/\w+/g) || { length: 0 };
   var numbersMatch = text.match(/[0-9]/g) || { length: 0 };
   var spacesMatch = text.match(/\s/g) || { length: 0 };
+  var paragraphsMatch = text.split(/\n+/) || { length: 0 };
   return {
     characters: text.length,
-    words: text.split(" ").filter(function(word) {
-      return word.length > 0;
-    }).length,
+    words: wordsMatch.length,
     spaces: spacesMatch.length,
-    numbers: numbersMatch.length
+    numbers: numbersMatch.length,
+    paragraphs: paragraphsMatch.filter(this.isWord).length
   };
 };
 
--- mocha/test_counter.js
+++ mocha/test_counter.js
@@ -4,6 +4,9 @@ var Counter = require("../counters/js/counter.js");
 var counts1 = Counter.count("TDD for the win");
 var counts2 = Counter.count("and BDD is better, y0");
 var counts3 = Counter.count("");
+var counts4 = Counter.count("this is p1\n\nthis is p2\n1 here");
+var counts5 = Counter.count("this is p1\n\n\n");
+var counts6 = Counter.count("word\nword");
 
 describe("Counter", function() {
   describe("count", function() {
@@ -16,6 +19,7 @@ describe("Counter", function() {
       expect(counts1.words).to.equal(4);
       expect(counts2.words).to.equal(5);
       expect(counts3.words).to.equal(0);
+      expect(counts6.words).to.equal(2);
     });
     it("counts spaces", function() {
       expect(counts1.spaces).to.equal(3);
@@ -24,6 +28,13 @@ describe("Counter", function() {
     it("counts numbers", function() {
       expect(counts1.numbers).to.equal(0);
       expect(counts2.numbers).to.equal(1);
+    });
+    it("counts paragraphs", function() {
+      expect(counts1.paragraphs).to.equal(1);
+      expect(counts2.paragraphs).to.equal(1);
+      expect(counts3.paragraphs).to.equal(0);
+      expect(counts4.paragraphs).to.equal(3);
+      expect(counts5.paragraphs).to.equal(1);
     });
   });
 });
