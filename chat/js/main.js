e905f10d93b3f45aac3a32234f3548841ae87c79
Collin Webb <collindwebb@gmail.com>
2015-06-13 14:32:23 -0700

    use clone to generate divs

--- chat/js/main.js
+++ chat/js/main.js
@@ -17,15 +17,16 @@ var sendMessage = function(textValue) {
   }
 };
 
-ref.on("child_added", function(snapshot) {
+var $messageTemplate = $(".text:first");
+
+ref.on("child_added", function(snapshot){
   var newMessage = snapshot.val();
-  if (newMessage.author && newMessage.message
-       && newMessage.message.trim().length > 0) {
-    var messageDiv = $("<div>")
-    .append($("<span>", { class: 'time', text: moment(newMessage.messageDate).format('lll') }))
-    .append($("<span>", { class: 'author', text: newMessage.author }))
-    .append($("<span>", { class: 'body', html: marked(newMessage.message) }))
-    $("#chat").prepend(messageDiv);
+  if (newMessage.author && newMessage.message && newMessage.message.trim().length > 0){
+    var $messageDiv = $messageTemplate.clone().removeClass("hidden")
+    $messageDiv.find(".time").text(moment(newMessage.messageDate).format("lll"));
+    $messageDiv.find(".author").text(newMessage.author);
+    $messageDiv.find(".message").html(marked(newMessage.message));
+    $("#chat").prepend($messageDiv);
   }
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
