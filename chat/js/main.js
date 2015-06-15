ed135e6f87191b39e5f2b091d6e0f8e70b93cad8
Collin Webb <collindwebb@gmail.com>
2015-06-14 19:30:39 -0700

    chat now previews messages and allows for shift-enter

--- chat/js/main.js
+++ chat/js/main.js
@@ -9,11 +9,23 @@ ref.authWithOAuthPopup("github", function(error, authData) {
   }
 }, {scope: 'user'});
 
+var $messageTemplate = $(".text:first");
+
 var initChat = function(user) {
   $("textarea").on("keyup", function(event) {
-    if (event.which === 13) {
+    if (event.which !== 13 || event.shiftKey) {
+      var $messageDiv = $messageTemplate.clone().removeClass("hidden").addClass("preview");
+      $messageDiv.find(".time").text(moment(Date.now()).format("lll"));
+      $messageDiv.find(".author").text(user.displayName);
+      $messageDiv.find(".message").html(marked(this.value));
+      $messageDiv.find(".avatar").attr("src", user.cachedUserProfile.avatar_url);
+      $("#preview").html($messageDiv);
+    } else {
       sendMessage(this.value);
       this.value = "";
+    }
+    if (this.value === "") {
+      $("#preview").empty();
     }
   });
 
@@ -26,8 +38,6 @@ var initChat = function(user) {
       });
     }
   };
-
-  var $messageTemplate = $(".text:first");
 
   ref.on("child_added", function(snapshot){
     var newMessage = snapshot.val();
