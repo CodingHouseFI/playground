9248f984f96706411aa0fd592e22a192cdba050c
Collin Webb <collindwebb@gmail.com>
2015-06-23 11:15:10 -0700

    makes search

--- tablePlugin/js/tableizer.js
+++ tablePlugin/js/tableizer.js
@@ -1,17 +1,20 @@
-$.fn.tableizer = function(data) {
+$.fn.tableizer = function(options) {
+  var data = options.data;
+  var search = options.search;
+
   this.each(function(i, e) {
     var $table = $("<table class='table'><thead class='table-head'> <tr> </tr> </thead> <tbody class='table-body'> </tbody> </table>");
 
     var userKeys = Object.keys(data[0]);
     var $headers = userKeys.map(function(e, i) {
-      return $("<th>", { text: e, class: "header", data: { direction: 'asc' }  })
+      return $("<th>", { text: e, class: "header", data: { direction: 'asc' }  });
     });
 
     $table.find("thead tr").html($headers);
     var sortData = {
       asc: { opposite: 'desc', direction: -1 },
       desc: { opposite: 'asc', direction: 1 },
-    }
+    };
     var directionKey;
     $table.on("click", "thead th", function() {
       var $th = $(this);
@@ -19,15 +22,22 @@ $.fn.tableizer = function(data) {
       $th.data("direction", sortData[directionKey].opposite);
       var keyForSort = $th.text();
       data.sortUsing(keyForSort, sortData[directionKey].direction);
-      redrawTheTableBody();
+      redrawTheTableBody(data);
     });
 
     $(e).html($table);
 
-    function redrawTheTableBody() {
+    $("#search").on("keypress", function (){
+      var find = data.filter(function(object){
+        return object.match(this.value);
+      });
+      // redrawTheTableBody(find);
+    });
+
+    function redrawTheTableBody(data) {
       var $rows = data.map(function(user) {
         var $rowData = userKeys.map(function(key, i) {
-          return $("<td>", { text: user[key], class: "data-value" })
+          return $("<td>", { text: user[key], class: "data-value" });
         });
         return $("<tr></tr>").html($rowData);
       });
@@ -35,14 +45,32 @@ $.fn.tableizer = function(data) {
       $(e).find("tbody").html($rows);
     }
 
-    redrawTheTableBody();
+    if (search){
+      drawSearchBox();
+    }
+
+    function drawSearchBox(){
+      var $input = $("<input id='search-box' type='text'>");
+      var timerId;
+      $input.on("keyup", function(){
+        var searchPhrase = this.value;
+        clearTimeout(timerId);
+        timerId = setTimeout(function(){
+          var filteredData = data.filter(function(user){
+            return userKeys.some(function(key){
+              return user[key].toString().match(searchPhrase);
+            });
+          });
+          redrawTheTableBody(filteredData);
+        }, 300);
+      });
+      $(e).prepend($input);
+    }
+
+    redrawTheTableBody(data);
   });
 };
 
-// var sortOrder = {
-//   asc: { opposite: 'desc', direction: -1 },
-//   desc: { opposite: 'asc', direction: 1 },
-// }
 Array.prototype.sortUsing = function(key, direction) {
   this.sort(function(a,b) {
     if (a[key] === b[key]) {
@@ -50,4 +78,4 @@ Array.prototype.sortUsing = function(key, direction) {
     }
     return direction * (a[key] > b[key] ? 1 : -1);
   });
-}
+};
