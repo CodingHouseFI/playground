558de5a294833f83441d52763bdc20f927635bdf
AgileLabs <samer@agilelabs.com>
2015-06-23 17:57:24 -0700

    Prep for FS

--- node-tests/test.js
+++ node-tests/test.js
@@ -1,25 +1,38 @@
 var fs = require("fs"),
     http = require("http"),
-    url = require('url');
+    request = require('request'),
+    url = require('url'),
+    exec = require("child_process").exec;
 
-function sleep(seconds, callback) {
-  setTimeout(callback, seconds * 1000);
-}
 
-http.createServer(responseHandler()).listen(8888);
+// exec("df -h", function(err, stdout, stderr) {
+//   console.log(err, stdout, stderr);
+// });
 
-function responseHandler(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  if (request.url.match("fav")) {
-    response.end("");
+http.createServer(responseHandler).listen(8888);
+
+function responseHandler(req, res) {
+  if (req.url.match("fav")) {
+    res.end("");
     return;
   }
 
-  var queryData = url.parse(request.url, true).query;
-  queryData.date = Date.now();
+  // exec("vm_stat", function(err, stdout, stderr) {
+  //   res.end(stdout.match(/free:\s+(\d+)\./)[1]);
+  // });
 
-  sleep(queryData.sleep || 0, function() {
-    response.write(JSON.stringify(queryData));
-    response.end();
-  });
+  if (req.url === "/") {
+    // server index.html
+    res.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile('index.html', 'utf8', function (err,data) {
+      res.end(data);
+    });
+  } else {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    request('http://points.agilelabs.com'+req.url+'.json', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.end(body);
+      }
+    });
+  }
 }
